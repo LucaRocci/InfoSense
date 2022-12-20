e412efed86238c8d0357d14a1fde6c5fb325ed8d
DESKTOP-U74VE9T\simon <simonesporeni@gmail.com>
2022-12-20 11:53:18 +0100

    [STYLE] Add style to mobile navbar

--- preditco-frontend/src/components/Navbar/Navbar.component.tsx
+++ preditco-frontend/src/components/Navbar/Navbar.component.tsx
@@ -1,8 +1,15 @@
-//Bootstrap components imports
+//React core imports
 import { useEffect, useState } from 'react';
+//Bootstrap components imports
 import { Navbar, Nav, Container, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import logo from '../../assets/logos/logo-predicto.svg';
+import { Github } from 'react-bootstrap-icons';
+//Import logos 
+import {ReactComponent as PredictoLogo} from '../../assets/logos/logo-predicto.svg';
+import {ReactComponent as PredictoLogoWhite} from '../../assets/logos/logo-predicto-white.svg';
+import {ReactComponent as InfosenseLogo} from '../../assets/logos/logo-infosense-navbar.svg';
+import {ReactComponent as IstatLogo} from '../../assets/logos/logo-istat.svg';
+
 //Import style
 import './Navbar.scss'
 
@@ -11,32 +18,45 @@ const NavbarCustom = () => {
 
   const [ show, setShow ] = useState<boolean>(false);
 
+  const handleToggle = () => {
+    if(show){
+    setTimeout(() => {
+      setShow(!show)
+    },300)
+  }else
+   setShow(!show)
+  }
+
   useEffect(() => {
     document.body.classList.toggle('overflow-y-hidden');
   }, [show]) 
 
     return(
-        <Navbar bg="light" expand="md" className='fixed-top'>
+        <Navbar expand="md" className={`fixed-top ${show?'bg-blue':'bg-skyblue'}`}>
         <Container fluid>
-          <Navbar.Brand><img src={logo} /></Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setShow(!show)} />
+          <Navbar.Brand>{show?<PredictoLogoWhite />:<PredictoLogo className='w-75' />}</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={handleToggle} />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className={`ms-md-auto ${show?'d-flex flex-column align-items-center min-h-90':''}`}>
-              <Nav.Link><Link to='/home'>Home</Link></Nav.Link>
-              <Nav.Link><Link to='/statistics'>Statistics</Link></Nav.Link>
-              <Nav.Link><Link to='/predictions'>Predictions</Link></Nav.Link>
+            <Nav className={`ms-md-auto text-white ${show?'min-h-90':''}`}>
+              <Link className={`nav-link text-white nav-link-font ${show?'mt-4 mx-2':''}`} to='/home'>Home</Link>
+              <Link className={`nav-link text-white nav-link-font ${show?'m-2':''}`} to='/statistics'>Statistics</Link>
+              <Link className={`nav-link text-white nav-link-font ${show?'m-2':''}`} to='/predictions'>Predictions</Link>
+              {show? 
+              <Row className='d-flex flex-row align-items-center mt-4'>
+                <Github className='nav-svg-github ms-2' /> <IstatLogo className='nav-svg-istat' />
+              </Row>:''}
               {show?
-              <Container fluid className='mt-auto'>
-                <Row>
-                  <Col className='text-center'>A</Col>
-                  <Col className='text-center'>B</Col>
+              <footer className='mt-auto py-3 nav-footer'>
+                <Row className='mb-4'>
+                  <Col className='text-center'>Privacy</Col>
+                  <Col className='text-center'>Contact</Col>
+                  <Col className='text-center'>Terms</Col>
                 </Row>
-                <Row>
-                  <Col className='text-center'>C</Col>
-                  <Col className='text-center'>D</Col>
-                  <Col className='text-center'>E</Col>
+                <Row className='mb-3'>
+                  <Col className='text-muted d-flex justify-content-start nav-copyrigth-text'>2022 @ all right reserved</Col>
+                  <Col className='d-flex justify-content-end'><InfosenseLogo className='w-75' /></Col>
                 </Row>
-              </Container>:''}
+              </footer>:''}
             </Nav>
           </Navbar.Collapse>
         </Container>
