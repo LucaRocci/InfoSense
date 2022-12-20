ee073b1e535c1d5e72f2686861c2dff89d85c680
SPOR3\simon <simonesporeni@gmail.com>
2022-12-20 10:17:26 +0100

    [STYLE] start styling navbar

--- preditco-frontend/src/components/Navbar/Navbar.component.tsx
+++ preditco-frontend/src/components/Navbar/Navbar.component.tsx
@@ -1,21 +1,42 @@
 //Bootstrap components imports
-import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
+import { Navbar, Nav, Container, Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import logo from '../../assets/logos/logo-predicto.svg';
 //Import style
 import './Navbar.scss'
 
 
 const NavbarCustom = () => {
+
+  const [ show, setShow ] = useState<boolean>(false);
+
+  useEffect(() => {
+    document.body.classList.toggle('overflow-y-hidden');
+  }, [show]) 
+
     return(
-        <Navbar bg="light" expand="lg">
+        <Navbar bg="light" expand="md" className='fixed-top'>
         <Container fluid>
           <Navbar.Brand><img src={logo} /></Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setShow(!show)} />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <Nav.Link>Home</Nav.Link>
-              <Nav.Link>Statistics</Nav.Link>
-              <Nav.Link>Predictions</Nav.Link>
+            <Nav className={`ms-md-auto ${show?'d-flex flex-column align-items-center min-h-90':''}`}>
+              <Nav.Link><Link to='/home'>Home</Link></Nav.Link>
+              <Nav.Link><Link to='/statistics'>Statistics</Link></Nav.Link>
+              <Nav.Link><Link to='/predictions'>Predictions</Link></Nav.Link>
+              {show?
+              <Container fluid className='mt-auto'>
+                <Row>
+                  <Col className='text-center'>A</Col>
+                  <Col className='text-center'>B</Col>
+                </Row>
+                <Row>
+                  <Col className='text-center'>C</Col>
+                  <Col className='text-center'>D</Col>
+                  <Col className='text-center'>E</Col>
+                </Row>
+              </Container>:''}
             </Nav>
           </Navbar.Collapse>
         </Container>
