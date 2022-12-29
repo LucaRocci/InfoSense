052eb3abf8b4acb777e7bb06e57984ca5e5ba894
DESKTOP-U74VE9T\simon <simonesporeni@gmail.com>
2022-12-29 21:47:46 +0100

    [NEW FEATURE] Add useFetch hook and first set up statistics page

---
+++ preditco-frontend/src/hooks/useFetch/useFetch.hook.tsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from 'react';
+
+type dataResponse = {
+    anno: number,
+    mese?: number,
+    valore: number,
+    arrivoPresenza: string
+}
+
+const useFetch = (url: string) => {
+    const [apiData, setData] = useState<dataResponse[] | string[] | null>(null);
+    const [loading, setLoading] = useState<boolean | null>(null);
+    const [error, setError] = useState<boolean | null>(null);
+
+    useEffect(() => {
+        setLoading(true)
+        setData(null);
+        setError(null);
+
+        fetch(url , {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+              }
+            })
+            .then(async res => {
+                const resJson = await res.json();
+                setLoading(false);
+                setData(resJson);
+            })
+            .catch(err => {
+                setLoading(false)
+                setError(true)
+            })
+
+    }, [url])
+
+    return { apiData, loading, error }
+}
+
+export default useFetch;
--- preditco-frontend/src/pages/statistics/Statistics.page.tsx
+++ preditco-frontend/src/pages/statistics/Statistics.page.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+//React core imports
+import React, { useEffect, useState } from 'react';
+//Charjs impoprts
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -9,7 +11,10 @@ import {
   Legend,
 } from 'chart.js';
 import { Bar } from 'react-chartjs-2';
+//React-bootstrap import
 import { Container } from 'react-bootstrap';
+//Hooks imorts
+import useFetch from '../../hooks/useFetch/useFetch.hook';
 
 ChartJS.register(
     CategoryScale,
@@ -20,7 +25,7 @@ ChartJS.register(
     Legend
   );
 
-  export const options = {
+ const options = {
     responsive: true,
     plugins: {
       legend: {
@@ -35,7 +40,7 @@ ChartJS.register(
 
   const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
 
-  export const data = {
+  const data = {
     labels,
     datasets: [
       {
@@ -58,9 +63,41 @@ ChartJS.register(
 
 const Statistics = () => {
 
+  const [ province, setProvince ] = useState<string>('Torino');
+  const [ activityType, setActivityType ] = useState<string>('alberghi');
+  const [ country, setCountry ] = useState<string>('Italia');
+
+  const { apiData, loading, error } = useFetch(`http://18.102.24.178:8000/statistics/${province}/${activityType}/${country}`) 
+
+  useEffect(() => {
+    console.log(apiData)
+  }, [apiData])
+
+/*   const handleSubmit = async (event: any) => {
+
+    event.preventDefault();
+    event.stopPropagation();
+
+    setProvince(event.target[0].value);
+    setActivityType(event.target[1].value);
+    setCountry(event.target[2].value)
+    console.log(province, activityType, country)
+
+  
+  } */
+
     return (
     <Container className='min-h-100 d-flex justify-content-center align-items-center'>
+{/*       <form onSubmit={handleSubmit}>
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+        {loading ? <>Loading...</>:''}
+        {error ? <>Error...</>:''}
+        <button type="submit">Submit</button>
+      </form> */}
         <div style={{height: '500px', width:'800px'}}>
+          <h2>{province}</h2>
     <Bar options={options} data={data} />
     </div>
     </Container>
