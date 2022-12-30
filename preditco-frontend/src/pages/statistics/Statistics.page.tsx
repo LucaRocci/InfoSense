f70cecfebb8191a73f5628da6db4d143d464c964
PietroMil <pietro.milanese@edu.itspiemonte.it>
2022-12-30 01:20:15 +0100

    [UPDATE]Modal form

--- preditco-frontend/src/pages/statistics/Statistics.page.tsx
+++ preditco-frontend/src/pages/statistics/Statistics.page.tsx
@@ -19,6 +19,10 @@ import useFetch from "../../hooks/useFetch/useFetch.hook";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
 import { ReactComponent as SettingIcon } from "../../assets/icons/setting.svg";
 import { ReactComponent as LineChartIcon } from "../../assets/icons/line-chart.svg";
 import { ReactComponent as PieChartIcon } from "../../assets/icons/pie-chart.svg";
@@ -125,7 +129,36 @@ const Statistics = () => {
         <Modal.Header closeButton>
           <Modal.Title>Modal heading</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Body>
+       
+     
+      <Form.Select aria-label="Default select example">
+      <option>Open this select menu</option>
+      <option value="1">One</option>
+      <option value="2">Two</option>
+      <option value="3">Three</option>
+    </Form.Select>
+
+      <InputGroup className="mb-3">
+        <DropdownButton
+          variant="outline-secondary"
+          title="Dropdown"
+          id="input-group-dropdown-1"
+        >
+          <Dropdown.Item >Action</Dropdown.Item>
+          <Dropdown.Item >Another action</Dropdown.Item>
+          <Dropdown.Item >Something else here</Dropdown.Item>
+       
+       
+        </DropdownButton>
+        <Form.Control aria-label="Text input with dropdown button" />
+      </InputGroup>
+
+     
+
+     
+
+        </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
