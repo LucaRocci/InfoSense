f91e64ae30f97103a6993d35dc7654ddb0908c99
Gabriele Bovolenta <gabriele.bovolenta.96@hotmail.com>
2023-01-09 14:16:41 +0100

    [UPDATE] Add total map year for api with month

--- preditco-frontend/src/hooks/useStcSingleYear.hook.tsx
+++ preditco-frontend/src/hooks/useStcSingleYear.hook.tsx
@@ -4,15 +4,16 @@ import { useState, useEffect } from "react";
 //React router dom imports
 import { useSearchParams } from "react-router-dom";
 
-//Import Type 
+//Import Type
 import { dataResponse } from "./useFetch.hook";
 import { DataChart, OptionChart } from "./useStcChart.hook";
 
 //hook for mapping the api response to chartjs valid object
-const useStcSingleYear = (apiData:dataResponse[] | string[] | null | boolean) : [data:DataChart , option:OptionChart ] => {
-
+const useStcSingleYear = (
+  apiData: dataResponse[] | string[] | null | boolean
+): [data: DataChart, option: OptionChart] => {
   //SearchParam hook
-  const [ searchParam ] = useSearchParams();
+  const [searchParam] = useSearchParams();
   //Single Chart setting state for year chart
   const [data, setData] = useState<DataChart>({
     labels: [],
@@ -26,126 +27,109 @@ const useStcSingleYear = (apiData:dataResponse[] | string[] | null | boolean) : 
         label: "",
         data: [],
         backgroundColor: "rgba(53, 162, 235, 0.5)",
-      }
+      },
     ],
   });
 
   //Char option state
-  const [option , setOption ] = useState<OptionChart>({
+  const [option, setOption] = useState<OptionChart>({
     maintainAspectRatio: false,
     plugins: {
       title: {
         display: true,
-        text: '',
+        text: "",
       },
       legend: {
         position: "top" as const,
-        display: false
+        display: false,
       },
       tooltip: {
         titleFont: {
-          size: 20
+          size: 20,
         },
         bodyFont: {
-          size: 20
+          size: 20,
         },
         footerFont: {
-          size: 10 // there is no footer by default
-        }
-      }
+          size: 10, // there is no footer by default
+        },
+      },
     },
   });
 
-    //Effect for change the chart settings on api change request
-    useEffect(() => {
+  //Effect for change the chart settings on api change request
+  useEffect(() => {
+    if (Array.isArray(apiData)) {
+      //Mapping data
+      const mappedYearArrival: any[] = apiData
+        .map((e) => {
+          if (typeof e === "string") return e;
+          else if (e.arrivoPresenza === "Arrivo" && e.mese === 0) {
+            return e.valore;
+          } else return 0;
+        })
+        .filter((e) => e !== 0);
 
-        if(Array.isArray(apiData)) {
-                            //Mapping data
-                            const mappedYearArrival:any[] = apiData.map((e) => {
-                                if(typeof e === 'string')
-                                   return e;
-                                else if(e.arrivoPresenza === 'Arrivo' && e.mese === 0) {
-                                    
-                                    return e.valore;
-                                }else return 0
-                            }).filter(e => e !== 0);
+      const mappedYearPres: any[] = apiData
+        .map((e) => {
+          if (typeof e === "string") return e;
+          else if (e.arrivoPresenza === "Presenza" && e.mese === 0) {
+            return e.valore;
+          } else return 0;
+        })
+        .filter((e) => e !== 0);
 
-                            const mappedYearPres:any[] = apiData.map((e) => {
-                                if(typeof e === 'string')
-                                   return e;
-                                else if(e.arrivoPresenza === 'Presenza' && e.mese === 0) {
-                                    return e.valore;
-                                }else return 0
-                            }).filter(e => e !== 0);
+      const mappedYearData = [mappedYearArrival[0], mappedYearPres[0]];
 
-                            const mappedYearData = [mappedYearArrival[0], mappedYearPres[0]];
+      const mappedMonthArrivals: any = apiData
+        .map((e) => {
+          if (typeof e === "string") return e;
+          else if (e.arrivoPresenza === "Arrivo" && e.mese !== 0) {
+            return e.valore;
+          } else return 0;
+        })
+        .filter((e) => e !== 0);
 
-                            const mappedMonthArrivals : any = apiData.map((e) => {
-                              if(typeof e === 'string')
-                                 return e;
-                              else if(e.arrivoPresenza === 'Arrivo' && e.mese !== 0) {
-                                  
-                                  return e.valore;
-                              }else return 0
-                          }).filter(e => e !== 0)
+      const mappedMonthPres: any = apiData
+        .map((e) => {
+          if (typeof e === "string") return e;
+          else if (e.arrivoPresenza === "Presenza" && e.mese !== 0) {
+            return e.valore;
+          } else return 0;
+        })
+        .filter((e) => e !== 0);
 
-                          const mappedMonthPres : any = apiData.map((e) => {
-                            if(typeof e === 'string')
-                               return e;
-                            else if(e.arrivoPresenza === 'Presenza' && e.mese !== 0) {
-                                return e.valore;
-                            }else return 0
-                        }).filter(e => e !== 0)
+      let monthSumArrivals: number = 0;
+      let monthSumPres: number = 0;
 
-                        let monthSumArrivals: any = [];
-                        let monthSumPres : any = [];
-                          
-                          if(mappedMonthArrivals.lenght !== 0 && mappedMonthPres.lenght !== 0) {
-                            monthSumArrivals = mappedMonthArrivals.reduce(
-                              (sum: string | number, num: string | number) => {
-                                if (typeof sum === 'number' && typeof num === 'number') {
-                                  return sum + num
-                                } else if (typeof sum === 'string'  && typeof num === 'string') {
-                                  return sum + num
-                                } else 
-                                 {
-                                  return num;
-                                 }
-                              })
+      if (mappedMonthArrivals.length !== 0 && mappedMonthPres.length !== 0) {
+        monthSumArrivals = mappedMonthArrivals.reduce((acc: number, current: number) => {
+          return acc + current;
+        }, 0);
+      
+        monthSumPres = mappedMonthPres.reduce((acc: number, current: number) => {
+          return acc + current;
+        }, 0);
+      }
+      const total = [monthSumArrivals, monthSumPres];
+      console.log(total);
+      
 
-                              monthSumPres = mappedMonthPres.reduce(
-                                (sum: string | number, num: string | number) => {
-                                  if (typeof sum === 'number' && typeof num === 'number') {
-                                    return sum + num
-                                  } else if (typeof sum === 'string'  && typeof num === 'string') {
-                                    return sum + num
-                                  } else 
-                                   {
-                                    return num;
-                                   }
-                                })
-                              } 
-                              
-                            const c = [monthSumArrivals, monthSumPres];
-
-        setData({
-          labels: ['Arrivi', 'Presenze'],
-          datasets: [
-            { 
-                data: mappedYearData.length === 0 ? c : mappedYearData,
-                backgroundColor: [
-                  "#4571eb",
-                  "#aa23c5"
-                ],
-                tension: 0.2
-              }
-          ],
-        });
-        };
-                
-}, [apiData]);
+      setData({
+        labels: ["Arrivi", "Presenze"],
+        datasets: [
+          {
+            /* data: mappedYearData.length === 0 ? total : mappedYearData, */
+            data: total,
+            backgroundColor: ["#4571eb", "#aa23c5"],
+            tension: 0.2,
+          },
+        ],
+      });
+    }
+  }, [apiData]);
   //Return the states
-  return [ data , option ];
+  return [data, option];
 };
 
 export default useStcSingleYear;
